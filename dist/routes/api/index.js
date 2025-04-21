bdec935124e9931d1414e52735e8a0021458072a
Sergio Arturo Aguilar Basurto <sergioaguilar@Laptop-de-Sergio.local>
2025-04-20 21:11:35 -0600

    Initial backend setup

---
+++ dist/routes/api/index.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+import assetsRoute from "./assets-routes.js";
+const router = Router();
+router.use("/assets", assetsRoute);
+export default router;
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+const router = Router();
+
+// import apiRoutes from './api/index.ts';
+router.get('/api/test/', (req, res) => {
+    res.json({ message: 'CORS funciona!' })});
+// router.use('/api', apiRoutes);
+
+export default router;
