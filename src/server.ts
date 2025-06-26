9cda723ca9d21fdc1ae3b3c413b093eb7f7358ca
Sergio Arturo Aguilar Basurto <sergioaguilar@Laptop-de-Sergio.local>
2025-06-25 19:47:00 -0600

    Updated cors origin

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://woof-quest.web.app'],
     credentials: true,
   }));
   
